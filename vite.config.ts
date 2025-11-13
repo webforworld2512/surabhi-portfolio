50ce865cec16688ad2a1b9336cb750d51c7e23e1
webforworld2512 <surabhichavan2512@gmail.com>
2025-11-13 14:34:47 -0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/my-portfolio',
+  base: '/surabhi-portfolio',
   server: {
     host: "::",
     port: 8080,
